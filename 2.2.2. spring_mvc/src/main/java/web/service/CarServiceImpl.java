7c9bec5ba065a3077c6bb0d331935df17ea35781
Roman <79163288+besSmertniyKoder@users.noreply.github.com>
2021-06-25 22:15:48 +0300

    0.0.1

---
+++ 2.2.2. spring_mvc/src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,24 @@
+package web.service;
+
+import web.model.Car;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarServiceImpl implements CarService{
+    private List<Car> carList = new LinkedList<>();
+    {
+        carList.add(new Car("lada","cumlina","a33fjjf"));
+        carList.add(new Car("bmw","ee","5555rgf"));
+        carList.add(new Car("olga","mohzga","plate"));
+        carList.add(new Car("monika","maine","nsdmf"));
+        carList.add(new Car("kia","korea","aboba"));
+    }
+
+    @Override
+    public List<Car> getCarByNum(int id) {
+        carList.stream().limit(id).collect(Collectors.toList());
+        return carList;
+    }
+}
